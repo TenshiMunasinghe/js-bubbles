ebd64c62b62f2df696b25dece02b5b0fdcff4409
shadow-chadow <56468853+shadow-chadow@users.noreply.github.com>
2020-05-15 13:32:28 +0530

    enlarges on hover

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 var container = document.getElementById('container');
+var positionBubbleRandomly = function (bubble, size) {
+    bubble.style.top = (window.innerHeight - size * 2) * Math.random() + size + 1 + "px";
+    bubble.style.left = (window.innerWidth - size * 2) * Math.random() + size + 1 + "px";
+};
 var createBubble = function () {
     var bubble = document.createElement('div');
     var size = Math.floor(Math.random() * (window.innerWidth / 7) + 50);
@@ -8,17 +12,33 @@ var createBubble = function () {
     bubble.style.width = bubble.style.height = size + "px";
     bubble.style.borderRadius = size / 2 + "px";
     bubble.style.background = "rgb(" + r + ", " + g + ", " + b + ")";
-    bubble.style.top = (window.innerHeight - size) * Math.random() + "px";
-    bubble.style.left = (window.innerWidth - size) * Math.random() + "px";
+    positionBubbleRandomly(bubble, size);
+    bubble.addEventListener('mouseover', function () {
+        bubble.style.width = bubble.style.height = size * 2 + "px";
+        bubble.style.borderRadius = size + "px";
+    });
+    bubble.addEventListener('mouseleave', function () {
+        bubble.style.width = bubble.style.height = size + "px";
+        bubble.style.borderRadius = size / 2 + "px";
+    });
     container.appendChild(bubble);
     setTimeout(function () { return (bubble.style.opacity = Math.random().toString()); }, 500);
     setInterval(function () {
-        bubble.style.top = (window.innerHeight - size * 2) * Math.random() + size + 1 + "px";
-        bubble.style.left = (window.innerWidth - size * 2) * Math.random() + size + 1 + "px";
+        positionBubbleRandomly(bubble, size);
     }, Math.random() * 500 + 1000);
     setTimeout(function () {
         bubble.style.opacity = '0';
-        setTimeout(function () { return bubble.remove(); }, 1000);
+        setTimeout(function () {
+            bubble.removeEventListener('mouseover', function () {
+                bubble.style.width = bubble.style.height = size * 2 + "px";
+                bubble.style.borderRadius = size + "px";
+            });
+            bubble.removeEventListener('mouseleave', function () {
+                bubble.style.width = bubble.style.height = size + "px";
+                bubble.style.borderRadius = size / 2 + "px";
+            });
+            bubble.remove();
+        }, 1000);
     }, Math.floor(Math.random() * 5000 + 5001));
 };
 setInterval(function () {
--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
 const container = <HTMLDivElement>document.getElementById('container')
 
+const positionBubbleRandomly = (bubble: HTMLDivElement, size: number) => {
+    bubble.style.top = `${
+        (window.innerHeight - size * 2) * Math.random() + size + 1
+    }px`
+    bubble.style.left = `${
+        (window.innerWidth - size * 2) * Math.random() + size + 1
+    }px`
+}
+
 const createBubble = () => {
     const bubble = document.createElement('div')
     const size = Math.floor(Math.random() * (window.innerWidth / 7) + 50)
@@ -10,23 +19,37 @@ const createBubble = () => {
     bubble.style.width = bubble.style.height = `${size}px`
     bubble.style.borderRadius = `${size / 2}px`
     bubble.style.background = `rgb(${r}, ${g}, ${b})`
-    bubble.style.top = `${(window.innerHeight - size) * Math.random()}px`
-    bubble.style.left = `${(window.innerWidth - size) * Math.random()}px`
+    positionBubbleRandomly(bubble, size)
+
+    bubble.addEventListener('mouseover', () => {
+        bubble.style.width = bubble.style.height = `${size * 2}px`
+        bubble.style.borderRadius = `${size}px`
+    })
+    bubble.addEventListener('mouseleave', () => {
+        bubble.style.width = bubble.style.height = `${size}px`
+        bubble.style.borderRadius = `${size / 2}px`
+    })
+
     container.appendChild(bubble)
     setTimeout(() => (bubble.style.opacity = Math.random().toString()), 500)
 
     setInterval(() => {
-        bubble.style.top = `${
-            (window.innerHeight - size * 2) * Math.random() + size + 1
-        }px`
-        bubble.style.left = `${
-            (window.innerWidth - size * 2) * Math.random() + size + 1
-        }px`
+        positionBubbleRandomly(bubble, size)
     }, Math.random() * 500 + 1000)
 
     setTimeout(() => {
         bubble.style.opacity = '0'
-        setTimeout(() => bubble.remove(), 1000)
+        setTimeout(() => {
+            bubble.removeEventListener('mouseover', () => {
+                bubble.style.width = bubble.style.height = `${size * 2}px`
+                bubble.style.borderRadius = `${size}px`
+            })
+            bubble.removeEventListener('mouseleave', () => {
+                bubble.style.width = bubble.style.height = `${size}px`
+                bubble.style.borderRadius = `${size / 2}px`
+            })
+            bubble.remove()
+        }, 1000)
     }, Math.floor(Math.random() * 5000 + 5001))
 }
 
